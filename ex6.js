408aed30e6ac0fd69fb3844c6c4616dc589ebd4e
juliatmuller <julia_t_muller@estudante.sesisenai.org.br>
2026-03-24 08:50:18 -0300

    add 6 e 7

---
+++ ex6.js
@@ -0,0 +1,13 @@
+
+function alfabeto() {
+
+    let letra = prompt("Digite uma letra do alfabeto : ");
+
+    if (letra === 'A' || letra === 'E' || letra === 'I' || letra === 'O' || letra === 'U') {
+        alert(" Sua letra é uma Vogal ");
+    } else {
+        alert(" Sua letra é uma Consoante ");
+
+    }
+
+}
---
+++ ex7.js
@@ -0,0 +1,40 @@
+function picoleteria(){
+ let codigo = prompt("Digite o codigo do sabor escolhido (A ate I): ");
+switch (codigo){
+        case 'a':
+        alert ("Picole de Chocolate, valor em R$ 1,50");
+        break;
+
+  case 'b':
+        alert ("Picole de Morango, valor em R$ 2,50");
+        break;
+
+          case 'c':
+        alert ("Picole de Creme, valor em R$ 2,50");
+        break;
+
+          case 'd' :
+        alert ("Picole de Manga, valor em R$ 3,20");
+        break;
+
+          case 'e' :
+        alert ("Picole de Melancia, valor em R$ 3,40");
+        break;
+
+          case 'f' :
+        alert ("Picole de Vanilla Ice, valor em R$ 3,00");
+        break;
+
+         case 'g' :
+        alert ("Picole de Céu Azul, valor em R$ 3,60");
+        break;
+
+         case 'h' :
+        alert ("Picole de Brownie, valor em R$ 4,00");
+        break;
+
+         case 'i' :
+        alert ("Picole de Hawaiano, valor em R$ 5,00");
+        break;
+}
+}
