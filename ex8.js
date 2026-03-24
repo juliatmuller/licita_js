f7ca8e92d7962a96853f32aeb9fd6866b4cc9d9a
juliatmuller <julia_t_muller@estudante.sesisenai.org.br>
2026-03-24 09:25:10 -0300

    add 8 e 9

---
+++ ex8.js
@@ -0,0 +1,10 @@
+function operacao() {
+let n_1 = 7;
+let n_2 = 9;
+
+alert (" O valor das variaveis é: 7 e 9");
+alert (" A diferença entre os numeros seria de: 2");
+alert (" O dobro do primeiro valor mais o triplo da segunda é: 41");
+alert (" O resultado da multiplicação dos variaeis é: 63");
+
+}
---
+++ ex9.js
@@ -0,0 +1,9 @@
+function decrescente() {
+let decrescente = [ 2 , 6 ];
+
+
+
+decrescente.sort((a,b) => b - a);
+alert("Ordem decrescente: " + decrescente );
+
+}
