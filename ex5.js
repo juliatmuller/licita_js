67b54526f5256ffe13ab34e8e6ca1b17185275b8
juliatmuller <julia_t_muller@estudante.sesisenai.org.br>
2026-03-24 07:52:24 -0300

    add

--- ex5.js
+++ ex5.js
@@ -1,5 +1,22 @@
 
-function transformaçõa(){
+function transformação(){
 
-  
+  for (let i =0; i <4; i++) {
+  numeros.push(Number(prompt("Digite um numero inteiro: ")));
+}
+ let numero = 10;
+if (numero % 2 === 0) {
+  console.log(Par);
+} else {
+  console.log(Ímpar);
+}
+
+let Par ={
+  soma = numero+1;
+}
+
+let ímpar ={
+  soma = numero-1;
+}
+
 }
